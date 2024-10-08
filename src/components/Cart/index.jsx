e310c4000f4ee9b3454a48dccf3ec8a4b4e0bd3e
LKrAxViCt0R <vickylakra10@gmail.com>
2024-10-09 02:14:23 +0530

    Created React query custom hook to implement parallel queries

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { useEffect, useState } from "react";
-
-import { NoData, Toastr } from "@bigbinary/neetoui";
-import productsApi from "apis/products";
+import { NoData } from "@bigbinary/neetoui";
 import { Header, PageLoader } from "components/commons";
 import { MRP, OFFER_PRICE } from "components/constants";
 import { cartTotalOf } from "components/utils";
+import { useFetchCartProducts } from "hooks/reactQuery/useProductsApi";
 import i18n from "i18next";
 import { isEmpty, keys } from "ramda";
 import useCartItemsStore from "stores/useCartItemsStore";
@@ -15,45 +13,12 @@ import PriceCard from "./PriceCard";
 import ProductCard from "./ProductCard";
 
 const Cart = () => {
-  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
-  const slugs = keys(cartItems);
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const slugs = useCartItemsStore(store => keys(store.cartItems));
+
+  const { data: products = [], isLoading } = useFetchCartProducts(slugs);
 
   const totalMrp = cartTotalOf(products, MRP);
   const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
-
-  const fetchCartProducts = async () => {
-    try {
-      const responses = await Promise.all(
-        slugs.map(slug => productsApi.show(slug))
-      );
-
-      setProducts(responses);
-
-      responses.forEach(({ availableQuantity, name, slug }) => {
-        if (availableQuantity >= cartItems[slug]) return;
-
-        setSelectedQuantity(slug, availableQuantity);
-        if (availableQuantity === 0) {
-          Toastr.error(
-            `${name} is no longer available and has been removed from cart`,
-            {
-              autoClose: 2000,
-            }
-          );
-        }
-      });
-    } catch (error) {
-      console.log("An error occurred:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCartProducts();
-  }, [cartItems]);
 
   if (isLoading) return <PageLoader />;
 
--- src/hooks/reactQuery/useProductsApi.js
+++ src/hooks/reactQuery/useProductsApi.js
@@ -1,16 +1,50 @@
-import productsApi from "apis/products";
+/* eslint-disable prettier/prettier */
 import { QUERY_KEYS } from "constants/query";
-import { useQuery } from "react-query";
 
-export const useShowProduct = slug => 
-    useQuery({
-        queryKey: [QUERY_KEYS.PRODUCTS, slug],
-        queryFn: () => productsApi.show(slug),
-    });
+import { existsBy } from "@bigbinary/neeto-cist";
+import { Toastr } from "@bigbinary/neetoui";
+import productsApi from "apis/products";
+import { prop } from "ramda";
+import { useTranslation } from "react-i18next";
+import { useQueries, useQuery } from "react-query";
+import useCartItemsStore from "stores/useCartItemsStore";
 
-export const useFetchProducts = params => 
-    useQuery({
-        queryKey: [QUERY_KEYS.PRODUCTS, params],
-        queryFn: () => productsApi.fetch(params),
-        keepPreviousData: true,
-    });
+export const useShowProduct = slug =>
+  useQuery({
+    queryKey: [QUERY_KEYS.PRODUCTS, slug],
+    queryFn: () => productsApi.show(slug),
+  });
+
+export const useFetchProducts = params =>
+  useQuery({
+    queryKey: [QUERY_KEYS.PRODUCTS, params],
+    queryFn: () => productsApi.fetch(params),
+    keepPreviousData: true,
+  });
+
+export const useFetchCartProducts = slugs => {
+  const { t } = useTranslation();
+  const { cartItems, setSelectedQuantity } = useCartItemsStore();
+
+  const responses = useQueries(
+    slugs.map(slug => ({
+      queryKey: [QUERY_KEYS.PRODUCTS, slug],
+      queryFn: () => productsApi.show(slug),
+      onSuccess: ({ availableQuantity, name }) => {
+        if (availableQuantity >= cartItems[slug]) return;
+
+        setSelectedQuantity(slug, availableQuantity);
+        if (availableQuantity === 0) {
+          Toastr.error(t("product.error.removedFromCart", { name }), {
+            autoClose: 2000,
+          });
+        }
+      },
+    }))
+  );
+
+  const data = responses.map(prop("data")).filter(Boolean);
+  const isLoading = existsBy({ isLoading: true }, responses);
+
+  return { data, isLoading };
+};
